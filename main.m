fae64760c8feacabab27ff1d4bf0d35590f9e59c
yichen <48315247+CFP106022212@users.noreply.github.com>
2020-05-26 17:17:08 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -67,7 +67,14 @@ for i = 1:length(result)
         result(i,4) = 2;
     end 
 end
-
+result = [ones(length(result),1)*N(6) result];
+try
+    global meshpoint;
+    meshpoint = [meshpoint;result];
+catch
+    global meshpoint;
+    meshpoint = result;
+end
 
 
 % --- Executes on button press in pushbutton2.
@@ -86,3 +93,91 @@ dragzoom();
 % --- Executes on button press in pushbutton3.
 function pushbutton3_Callback(hObject, eventdata, handles)
 cla reset;
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+N = plotline(handles);
+posx     = N(1);
+posy     = N(2);
+r        = N(3);
+thetamin = N(4);
+thetamax = N(5);
+
+theta    = thetamin:0.001:thetamax;
+x        = r*cos(theta)+ posx;
+y        = r*sin(theta)+ posy;
+
+
+tick = 1;
+result = zeros(2,4);
+for i = floor(min(x)):ceil(max(x))
+    for j = floor(min(y)):ceil(max(y))
+        if angle((i-posx)+1i*(j-posy))>thetamin && angle((i-posx)+1i*(j-posy))<thetamax
+            if abs((j-posy)-sqrt(r^2-(i-posx)^2))<1 
+                result(tick,1:2) = [i,j];
+                if abs((i-posx)-sqrt(r^2-(j-posy)^2))<abs((i-posx)+sqrt(r^2-(j-posy)^2))
+                    result(tick,3:4) = [(i-posx)-sqrt(r^2-(j-posy)^2),(j-posy)-sqrt(r^2-(i-posx)^2)];
+                else 
+                    result(tick,3:4) = [(i-posx)+sqrt(r^2-(j-posy)^2),(j-posy)-sqrt(r^2-(i-posx)^2)];
+                end 
+                tick = tick+1;
+            elseif abs((j-posy)+sqrt(r^2-(i-posx)^2))<1 
+                result(tick,1:2) = [i,j];
+                if abs((i-posx)-sqrt(r^2-(j-posy)^2))<abs((i-posx)+sqrt(r^2-(j-posy)^2))
+                    result(tick,3:4) = [(i-posx)-sqrt(r^2-(j-posy)^2),(j-posy)+sqrt(r^2-(i-posx)^2)];
+                else 
+                    result(tick,3:4) = [(i-posx)+sqrt(r^2-(j-posy)^2),(j-posy)+sqrt(r^2-(i-posx)^2)];
+                end 
+                tick = tick+1;
+            elseif abs((i-posx)-sqrt(r^2-(j-posy)^2))<1  
+                result(tick,1:2) = [i,j];
+                if abs((j-posy)-sqrt(r^2-(i-posx)^2))<abs((j-posy)+sqrt(r^2-(i-posx)^2))
+                    result(tick,3:4) = [(i-posx)-sqrt(r^2-(j-posy)^2),(j-posy)-sqrt(r^2-(i-posx)^2)];
+                else 
+                    result(tick,3:4) = [(i-posx)-sqrt(r^2-(j-posy)^2),(j-posy)+sqrt(r^2-(i-posx)^2)];
+                end 
+                tick = tick+1;
+            elseif abs((i-posx)+sqrt(r^2-(j-posy)^2))<1  
+                result(tick,1:2) = [i,j];
+                if abs((j-posy)-sqrt(r^2-(i-posx)^2))<abs((j-posy)+sqrt(r^2-(i-posx)^2))
+                    result(tick,3:4) = [(i-posx)+sqrt(r^2-(j-posy)^2),(j-posy)-sqrt(r^2-(i-posx)^2)];
+                else 
+                    result(tick,3:4) = [(i-posx)+sqrt(r^2-(j-posy)^2),(j-posy)+sqrt(r^2-(i-posx)^2)];
+                end 
+                tick = tick+1;
+            end 
+        end 
+    end 
+end 
+
+switch N(6)
+    case 1
+        result = result(result(:,4)>=0,:);
+    case 2
+        result = result(result(:,4)<=0,:);
+    case 3
+        result = result(result(:,3)<=0,:);
+    case 4
+        result = result(result(:,3)>=0,:);
+end
+
+for i = 1:length(result)
+    if result(i,3)>1
+        result(i,3) = 2;
+    end 
+    if result(i,4)>1
+        result(i,4) = 2;
+    end
+end
+result = [ones(length(result),1)*N(7) result];
+try
+    global meshpoint;
+    meshpoint = [meshpoint;result];
+catch
+    global meshpoint;
+    meshpoint = result;
+end
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
