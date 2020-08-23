de008991972233585d95f2bc4a52e6b52d68bfe4
CFP106022212 <ss106022212@gapp.nthu.edu.tw>
2020-08-23 12:31:48 +0800

    puzzle

---
+++ addline.m
@@ -0,0 +1,18 @@
+function output = addline(x,y,len,angle,posistion,type)
+    if angle == 90 || angle == 270 || angle == -90
+        y0 = y + len*sin(angle/180*pi);
+        ymin = min([y,y0]);
+        ymax = max([y,y0]);
+        output = [0,0,0,0,1,-x,x-1,x+1,ymin,ymax,0,360,posistion,type];
+    else
+        a  = tan(angle/180*pi);
+        b  = y-a*x;
+        x0 = x+len*cos(angle/180*pi);
+        y0 = y+len*sin(angle/180*pi);
+        xmin = min([x,x0]);
+        ymin = min([y,y0]);
+        xmax = max([x,x0]);
+        ymax = max([y,y0]);
+        output = [0,0,0,-1,a,b,xmin,xmax,ymin,ymax,0,360,posistion,type];
+    end
+end
