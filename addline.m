c670bb7ef744e0ecc2c783659b9f0e3e22fd023d
CFP106022212 <ss106022212@gapp.nthu.edu.tw>
2020-08-23 13:34:33 +0800

    upload

--- addline.m
+++ addline.m
@@ -1,9 +1,9 @@
-function output = addline(x,y,len,angle,posistion,type)
+function output = addline(x,y,len,angle,position,type)
     if angle == 90 || angle == 270 || angle == -90
         y0 = y + len*sin(angle/180*pi);
         ymin = min([y,y0]);
         ymax = max([y,y0]);
-        output = [0,0,0,0,1,-x,x-1,x+1,ymin,ymax,0,360,posistion,type];
+        output = [0,0,0,0,1,-x,x-1,x+1,ymin,ymax,0,360,position,type];
     else
         a  = tan(angle/180*pi);
         b  = y-a*x;
@@ -13,6 +13,6 @@ function output = addline(x,y,len,angle,posistion,type)
         ymin = min([y,y0]);
         xmax = max([x,x0]);
         ymax = max([y,y0]);
-        output = [0,0,0,-1,a,b,xmin,xmax,ymin,ymax,0,360,posistion,type];
+        output = [0,0,0,-1,a,b,xmin,xmax,ymin,ymax,0,360,position,type];
     end
 end
